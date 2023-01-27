7244de7bdcb98087672de727467eab48790fb6a1
Edison Estival Restrepo Ospina <edisonestival@gmail.com>
2023-01-27 11:12:29 -0500

    Se realiza tarea#2

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +8,34 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+/**
+ * HTML tiene una lista vacía ul#ingredients.
+ */
+const list = document.querySelector('#ingredients');
+
+/**
+ * 1. Cree un elemento separado <li>. Asegúrese de usar el método document.createElement().
+ */
+const item = document.createElement('li');
+
+/**
+ * 2. Añada el nombre de un ingrediente como contenido de texto.
+ */
+item.textContent = ingredients[0];
+list.append(item);
+
+/**
+ * 3. Añada una clase item al elemento.
+ */
+item.innerHTML = '<li class="item"></li>';
+
+/**
+ * 4. Después, inserte todos los <li> en una sola operación en la lista ul#ingredients.
+ */
+const addElements = ingredients.map((ingredient) => `<li class="item">${ingredient}</li>`).join("");
+console.log(addElements);
+list.innerHTML = addElements;
+
+
