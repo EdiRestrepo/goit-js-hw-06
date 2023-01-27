0f23d28b5cb562cbff7bcb4be32a2a05ca97a9f2
Edison Estival Restrepo Ospina <edisonestival@gmail.com>
2023-01-27 12:19:16 -0500

    Se realiza tarea #3

--- js/task-03.js
+++ js/task-03.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +14,32 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+/**
+ * El HTML tiene una lista ul.gallery.
+ */
+const listGallery = document.querySelector('.gallery');
+
+/**
+ * Use un array de objetos images para crear elementos <img> anidados dentro de <li>. Use las secuencias 
+ * de plantillas y el método insertAdjacentHTML() para crear el marcado.
+ */
+const gallery = images.map(image => `<li><img src="${image.url}" alt="${image.alt}" width="270" height="210"></li>`).join("");
+
+/**
+ * Todos los elementos de la galería deben añadirse al DOM en una sola operación de inserción.
+ */
+listGallery.insertAdjacentHTML("afterbegin", gallery); 
+
+/**
+ * Añada un diseño mínimo a la galería con flexboxes o Computación en malla mediante las clases CSS.
+ */
+listGallery.style.listStyle = "none";
+listGallery.style.display = "flex";
+listGallery.style.gap = "30px";
+listGallery.style.justifyContent = "center";
+listGallery.style.alingContent = "center";
+listGallery.style.position = "absolute";
+listGallery.style.top = "50%";
+listGallery.style.left = "50%";
+listGallery.style.transform = "translate(-50%, -50%)";
